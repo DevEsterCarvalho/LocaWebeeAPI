7665cb4b30d39dbfc9c125925431417d5970c995
DevEsterCarvalho <estercarvalho205@gmail.com>
2024-09-09 20:25:14 -0300

    conectando com banco local

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -8,7 +8,7 @@ namespace LocaWebee.Data
         public DbSet<Usuario> Usuarios { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseInMemoryDatabase("locawebee");
+        => options.UseSqlServer("Data Source=(LocalDb)\\MSSQLLocalDB;Database=locawebee;Integrated Security=True;Persist Security Info=False;Pooling=False;Multiple Active Result Sets=False;Connect Timeout=60;Encrypt=True;Trust Server Certificate=False;Command Timeout=0");
             
     }
 }
